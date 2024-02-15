c3f632dbe46f4ffe7d54737391568ea59fe22345
Desislav Arashev <desislav.arashev@gmail.com>
2024-02-16 00:05:26 +0200

    Update eslint configuration and add click event to Square component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React, { useState } from 'react'
 
 // eslint-disable-next-line react/prop-types
-function Square ({ value }) {
-  return <button className="square" >{value}</button>
+function Square({ value, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick} >{value}</button>
 }
 
-export default function Board () {
+export default function Board() {
+  function handleClick(i) {
+    const nextSquares = squares.slice()
+    nextSquares[i] = 'X'
+    setSquares(nextSquares)
+  }
+
   // eslint-disable-next-line no-unused-vars
   const [squares, setSquares] = useState(Array(9).fill(null))
   return (
     <>
       <div className="board-row">
-        <Square value={[squares[1]]} />
-        <Square value={[squares[2]]} />
-        <Square value={[squares[3]]} />
+        <Square value={[squares[1]]} onSquareClick={() => handleClick(1)} />
+        <Square value={[squares[2]]} onSquareClick={() => handleClick(2)} />
+        <Square value={[squares[3]]} onSquareClick={() => handleClick(3)} />
       </div>
       <div className="board-row">
-        <Square value={[squares[4]]} />
-        <Square value={[squares[5]]} />
-        <Square value={[squares[6]]} />
+        <Square value={[squares[4]]} onSquareClick={() => handleClick(4)} />
+        <Square value={[squares[5]]} onSquareClick={() => handleClick(5)} />
+        <Square value={[squares[6]]} onSquareClick={() => handleClick(6)} />
       </div>
       <div className="board-row">
-        <Square value={[squares[7]]} />
-        <Square value={[squares[8]]} />
-        <Square value={[squares[9]]} />
+        <Square value={[squares[7]]} onSquareClick={() => handleClick(7)} />
+        <Square value={[squares[8]]} onSquareClick={() => handleClick(8)} />
+        <Square value={[squares[9]]} onSquareClick={() => handleClick(9)} />
       </div>
     </>
   )
